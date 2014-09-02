7c9236261bf702841d0026af6d20cab793bdd8f1
Bappa <iftakharul.alam@live.com>
2014-09-02 08:20:13 -0800

    sidebar blade file added

---
+++ app/views/layout/sidebar.blade.php
@@ -0,0 +1,33 @@
+<div id="sidebar">
+    <div id="search">
+        <input type="text" placeholder="Search here..."/><button type="submit" class="tip-right" title="Search"><i class="fa fa-search"></i></button>
+    </div>
+    <ul>
+        <li><a href="reference"><i class="fa fa-home"></i> <span>Home</span></a></li>
+        <li class="submenu">
+            <a href="#"><i class="fa fa-th-list"></i> <span>Student</span> <i class="arrow fa fa-chevron-right"></i></a>
+            <ul>
+                <li><a href="/profile"><span class="badge badge-danger pull-right"> Update Required </span>Profile</a></li>
+                <li><a href="#">Photo</a></li>
+                <li><a href="#">Result</a></li>
+                <li><a href="#"><span class="badge badge-purple pull-right"> 3 </span>Notice</a></li>
+            </ul>
+        </li>
+        <li class="submenu active">
+            <a href="#"><i class="fa fa-th-list"></i><span>Reference</span> <i class="arrow fa fa-chevron-right"></i></a>
+            <ul>
+                <li><a href="audio"><span class="badge badge-purple pull-right"> 5 Audios Added </span>Audios</a></li>
+                <li><a href="video">Videos</a></li>
+                <li><a href="document"><span class="badge badge-purple pull-right"> 2 New Files </span>Documents</a></li>
+                <li><a href="add_resource">Add Resource</a></li>
+
+            </ul>
+        </li>
+
+        <!--        <li><a href="tables.html"><i class="fa fa-th-list"></i> <span>Tables</span></a></li>-->
+
+
+
+    </ul>
+
+</div>
