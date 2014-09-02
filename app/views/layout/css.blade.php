17ea761f4d3402f5020ba52f17b878c05d7a012b
Bappa <iftakharul.alam@live.com>
2014-09-02 08:17:24 -0800

    css added

---
+++ app/views/layout/css.blade.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}" />
+<link rel="stylesheet" href="{{ URL::asset('css/font-awesome.css') }}" />
+<link rel="stylesheet" href="{{ URL::asset('css/colorpicker.css') }}" />
+<link rel="stylesheet" href="{{ URL::asset('css/datepicker.css') }}" />
+<link rel="stylesheet" href="{{ URL::asset('css/icheck/flat/blue.css') }}" />
+<link rel="stylesheet" href="{{ URL::asset('css/select2.css') }}" />
+<link rel="stylesheet" href="{{ URL::asset('css/jquery-ui.css') }}" />
+<link rel="stylesheet" href="{{ URL::asset('css/unicorn.css') }}" />
